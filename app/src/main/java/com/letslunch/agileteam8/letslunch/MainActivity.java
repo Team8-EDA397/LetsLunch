878b482282330a597b4bb48334f62bab3b90d0c7
gswed <gustav.swedberg@gmail.com>
2017-04-04 12:29:53 +0200

    feat(issue 9): Added scheduled notification
    Modified MainActivity with sendNotification method.
    
    Added ReminderReceiver & ReminderAlarmService to handle scheduled
    notifications.

--- app/src/main/java/com/letslunch/agileteam8/letslunch/MainActivity.java
+++ app/src/main/java/com/letslunch/agileteam8/letslunch/MainActivity.java
@@ -1,13 +1,20 @@
 package com.letslunch.agileteam8.letslunch;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //start the notification service
+        sendNotification();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +60,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendNotification(){
+        Intent intent = new Intent(this, ReminderReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        // Set the alarm to start at approximately 7:30 p.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 19);
+        calendar.set(Calendar.MINUTE, 30);
+
+        //set it to repeat every day (not tested)
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() , AlarmManager.INTERVAL_DAY, pendingIntent);
+
+
     }
 }
---
+++ app/src/main/java/com/letslunch/agileteam8/letslunch/ReminderAlarmService.java
@@ -0,0 +1,40 @@
+package com.letslunch.agileteam8.letslunch;
+
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationManagerCompat;
+
+/**
+ * Created by Gustav on 2017-04-03.
+ */
+
+public class ReminderAlarmService extends IntentService {
+    private static final int NOTIFICATION_ID = 3;
+
+    public ReminderAlarmService() {
+        super("ReminderAlarmService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Lets Lunch reminder");
+        builder.setContentText("What's your lunch plan?");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //to be able to launch your activity from the notification
+        builder.setContentIntent(pendingIntent);
+        Notification notificationCompat = builder.build();
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFICATION_ID, notificationCompat);
+    }
+
+
+}
